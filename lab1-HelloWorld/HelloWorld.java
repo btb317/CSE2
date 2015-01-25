d7df3f06e94880ba02848c56f397851a1c73db1f
bbenedixlu <btb317@lehigh.edu>
2015-01-25 21:16:05 +0000

    updated HelloWorld

---
+++ lab1-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//Braeden Benedix
+//Lab 1
+//Hello World
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  then run the program
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+        //print the statement with the box around it
+        System.out.println("##############################");
+        System.out.println("##############################");
+        System.out.println("#                            #");
+        System.out.println("#      Hello World 2015      #");
+        System.out.println("#                            #");
+        System.out.println("##############################");
+        System.out.println("##############################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}

ed0d8fc9412018a773957aefbdbde42febcb26ef
bbenedixlu <btb317@lehigh.edu>
2015-01-28 03:15:30 +0000

    submit hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Braeden Benedix
+//CSE002
+//Prof. Chen
+//hw01
+//HelloCLass
+
+//define class
+public class WelcomeClass{
+    
+    //main method
+    public static void main(String[] args) {
+        
+        //print the welcome statement
+        System.out.println("    -----------");
+        System.out.println("    | Welcome |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-B--T--B--3--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("I am from southern Maine, and this summer I worked as a barback at a tavern in my hometown.");
+    }
+}

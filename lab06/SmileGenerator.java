0ecf4f7abb88b36dcac2505a986844ea62485ff0
bbenedixlu <btb317@lehigh.edu>
2015-03-25 03:22:15 +0000

    save lab06

---
+++ lab06/SmileGenerator.java
@@ -0,0 +1,20 @@
+//Braeden Benedix
+//lab06
+//SmileGenerator
+//CSE002
+//Prof Chen
+
+/*
+
+*/
+
+public class SmileGenerator {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.print(":)");
+        }
+        System.out.println("");
+    }
+}

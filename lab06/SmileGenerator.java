add43f3fc4fa7fd48cd1ebce9e8a12cc1cf06227
bbenedixlu <btb317@lehigh.edu>
2015-04-28 03:04:57 +0000

    submit lab06

--- lab06/SmileGenerator.java
+++ lab06/SmileGenerator.java
@@ -12,9 +12,58 @@ public class SmileGenerator {
     
     public static void main(String[] args) {
         
-        for (int i = 0; i < 5; i++) {
+        //Step 1
+        int i;
+        for (i = 0; i < 5; i++) {
             System.out.print(":)");
         }
         System.out.println("");
+        
+        i=0;
+        while (i<5) {
+            System.out.print(":)");
+            i++;
+        }
+        System.out.println("");
+        
+        i=0;
+        do {
+            System.out.print(":)");
+            i++;
+        } while(i<5);
+        System.out.println("\n");
+        
+        //Step 2
+        int rng = (int) (Math.random() * 100) + 1;
+        for (i=0; i<rng; i++) {
+            System.out.print(":)");
+        }
+        System.out.println("\n");
+        
+        //Step 3
+        int lineCounter = 0;
+        for (i=0; i<rng; i++) {
+            System.out.print(":)");
+            lineCounter++;
+            if (lineCounter == 30) {
+                System.out.println();
+                lineCounter = 0;
+            }
+        }
+        System.out.println("\n");
+        
+        //Step 4
+        lineCounter = 0;
+        int lineSize = 1;
+        for (i=0; i<rng; i++) {
+            System.out.print(":)");
+            lineCounter++;
+            if (lineCounter == lineSize) {
+                System.out.println();
+                lineCounter = 0;
+                lineSize++;
+            }
+        }
+        System.out.println();
     }
 }

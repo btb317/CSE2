a24220399628a31f331f747cc96af1835e0f056f
bbenedixlu <btb317@lehigh.edu>
2015-05-02 00:43:22 +0000

    update hw13

--- hw13/FourDwin.java
+++ hw13/FourDwin.java
@@ -46,11 +46,13 @@ public class FourDwin {
         int rand1 = rng.nextInt(Y - X + 1) + X;
         int rand2 = rng.nextInt(Y - X + 1) + X;
         int rand3 = rng.nextInt(Y - X + 1) + X;
-        double[][][][] array = new double[3][rand1][rand2][rand3];
+        double[][][][] array = new double[3][rand1][rand2][];
+        
         for (int i=0; i<3; i++) {
             for (int i1=0; i1<rand1; i1++) {
                 for (int i2=0; i2<rand2; i2++) {
-                    for (int i3=0; i3<rand3; i3++) {
+                    array[i][i1][i2] = new double[rng.nextInt(Y - X + 1)+X];
+                    for (int i3=0; i3<array[i][i1][i2].length; i3++) {
                         array[i][i1][i2][i3] = (double) ((int) (Math.random() * 300)) / 10;
                     }
                 }
@@ -58,18 +60,20 @@ public class FourDwin {
         }
         
         printArray(array);
+        statArray(array);
+        //System.out.println(min3d(array[0]));
     }
     
     public static void printArray(double[][][][] array) {
         System.out.println("{");
-        for (int i=0; i<array.length(); i++) {
+        for (int i=0; i<array.length; i++) {
             System.out.println("\t{");
             System.out.print("\t\t");
-            for (int i1=0; i1<array[i].length(); i1++) {
+            for (int i1=0; i1<array[i].length; i1++) {
                 System.out.print("{");
-                for (int i2=0; i2<array[i1].length(); i2++) {
+                for (int i2=0; i2<array[i][i1].length; i2++) {
                     System.out.print("{");
-                    for (int i3=0; i3<array[i2].length(); i3++) {
+                    for (int i3=0; i3<array[i][i1][i2].length; i3++) {
                         System.out.print(array[i][i1][i2][i3]+", ");
                     }
                     System.out.print("}, ");
@@ -80,4 +84,69 @@ public class FourDwin {
         }
         System.out.println("}");
     }
+    
+    public static int numElements(double[][][] array) { //finds number of elements in a 3d array
+        int num = 0;
+        for (int i=0; i<array.length; i++) {
+            for (int i1=0; i1<array[i].length; i1++) {
+                num += array[i][i1].length;
+            }
+        }
+        return num;
+    }
+    
+    public static double sum3d(double[][][] array) {
+        double sum = 0;
+        for (int i=0; i<array.length; i++) {
+            for (int i1=0; i1<array[i].length; i1++) {
+                for (int i2=0; i2<array[i][i1].length; i2++) {
+                    sum += array[i][i1][i2];
+                }
+            }
+        }
+        return sum;
+    }
+    
+    public static void statArray(double[][][][] array) {
+        int eleNum=0;
+        double sum=0;
+        for (int i=0; i<3; i++) {
+            eleNum += numElements(array[i]);
+            sum += sum3d(array[i]);
+        }
+        System.out.println("Members:\t"+eleNum);
+        System.out.println("Sum:\t\t"+sum);
+        double mean = sum / eleNum;
+        System.out.println("Mean:\t\t"+mean);
+    }
+    
+    public static double min3d(double[][][] array) {
+        double min = 50;
+        for (int i=0; i<array.length; i++) {
+            for (int i1=0; i1<array[i].length; i1++) {
+                for (int i2=0; i2<array[i][i1].length; i2++) {
+                    if (array[i][i1][i2] < min) {
+                        min = array[i][i1][i2];
+                    }
+                }
+            }
+        }
+        return min;
+    }
+    
+    /*
+    public static double[][][][] sort4DArray(double[][][][] array) {
+        double key, j;
+        double[][][] temp;
+        for (int i=1; i<array.length; i++) {
+            key = min3d(array[i]);
+            temp = array[i];
+            j = i - 1;
+            while (j>-1 && min3d(array[j]) > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }*/
 }
